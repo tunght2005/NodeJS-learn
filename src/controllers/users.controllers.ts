d046e29a9da4d25902dfc66abafeffef319175c5
Tung DEV <tunght2005@gmail.com>
2026-07-22 22:23:57 +0700

    feat: add schema user and user services, fix connect database service

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import usersService from '~/services/users.services'
 
 // xử lí các logic và xử lí database
 export const loginController = (req: Request, res: Response) => {
@@ -12,4 +13,21 @@ export const loginController = (req: Request, res: Response) => {
   return res.status(400).json({
     error: 'Login failed'
   })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  console.log(req.body)
+  const { email, password } = req.body
+  try {
+    const result = await usersService.register({ email, password })
+    return res.status(200).json({
+      message: 'Register success',
+      result
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: 'Register failed',
+      error
+    })
+  }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { loginController } from '~/controllers/users.controllers'
+import { loginController, registerController } from '~/controllers/users.controllers'
 import { loginValidator } from '~/middlewares/users.middlewares'
 const userRouter = Router()
 
 // xử lí post login -> loginValidator -> loginController
 userRouter.post('/login', loginValidator, loginController)
-
+userRouter.post('/register', registerController)
 export default userRouter
