699022ac16894117d323211e0402318417c146b3
Tung DEV <tunght2005@gmail.com>
2026-07-24 23:33:57 +0700

    feat: handle validator register (name, email, password,...)

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
+import { checkSchema } from 'express-validator'
+import { validate } from '~/utils/validation'
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   // Nhận data định dạng json lấy data body gửi lên
@@ -12,3 +14,98 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
   }
   next()
 }
+
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: true, // Không được bỏ trống tên
+      isString: true, // Định dạng chuỗi
+      trim: true, // Loại bỏ khoảng trắng
+      isLength: {
+        options: {
+          min: 1,
+          max: 100
+        }
+      } // Độ dài name 1-100
+    },
+    email: {
+      notEmpty: true,
+      isEmail: true,
+      trim: true
+      //Nếu tốt thì tạo thêm 1 checkEmailExist để kiếm tra email có trùng với email trong db hay không
+      // Ví dụ tạo 1 hàm async checkEmailExist(email: string) {
+      //     const user = await database.users.findOne({
+      //         email
+      //     })
+
+      //     return Boolean(user)
+      // } Để kiểm tra email có trong db hay không ở folder services
+      // custom: {
+      //   options: async (value, { req }) => {
+      //     const isExist = await usersService.checkEmailExist(value)
+      //     if (isExist) {
+      //       throw new Error('Email already exists')
+      //     }
+      //     return true
+      //   }
+      // }
+    },
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      }
+      // Kiểm tra độ mạnh của mk
+      // isStrongPassword: {
+      //   options: {
+      //     minLength: 6,
+      //     minLowercase: 1,
+      //     minUppercase: 1,
+      //     minNumbers: 1,
+      //     minSymbols: 1
+      //   },
+      // errorMessage: 'Password not strong'
+      // }
+    },
+    confirm_password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      },
+      // isStrongPassword: {
+      //   options: {
+      //     minLength: 6,
+      //     minLowercase: 1,
+      //     minUppercase: 1,
+      //     minNumbers: 1,
+      //     minSymbols: 1
+      //   },
+      // errorMessage: 'Password not strong'
+      // }
+      custom: {
+        options: (value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Password confirmation does not match password')
+          }
+          return true
+        }
+      }
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
+      }
+    }
+  })
+)
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 const userRouter = Router()
 
 // xử lí post login -> loginValidator -> loginController
 userRouter.post('/login', loginValidator, loginController)
-userRouter.post('/register', registerController)
+
+/*
+ * Description: Register a new user
+ * Path: /register
+ * Methob: POST
+ * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601}
+ */
+userRouter.post('/register', registerValidator, registerController)
 export default userRouter
