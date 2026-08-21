ea207fc94755976edb6a569dc804bd0503374384
Tung DEV <tunght2005@gmail.com>
2026-08-21 10:13:27 +0700

    feat: handle minimist argument parsing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
+import { config } from 'dotenv'
 import userRouter from './routes/users.routes'
 import databaseService from '~/services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
+config()
 
 databaseService.connect()
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 // Tạo folder upload
 initFolder()
