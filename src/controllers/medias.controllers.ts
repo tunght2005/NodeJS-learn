88ca8b0b0ddf3cd4adccb7ec5d461f6873a72b2c
Tung DEV <tunght2005@gmail.com>
2026-08-21 11:44:29 +0700

    feat: upload video

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
 import path from 'path'
-import { UPLOAD_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
 
 export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
   const url = await mediasService.uploadImage(req)
@@ -12,9 +12,25 @@ export const uploadImageController = async (req: Request, res: Response, next: N
   })
 }
 
+export const uploadVideoController = async (req: Request, res: Response, next: NextFunction) => {
+  const url = await mediasService.uploadVideo(req)
+  return res.json({
+    message: USERS_MESSAGES.UPLOAD_SUCCESS,
+    result: url
+  })
+}
+
 export const serveImageController = (req: Request<{ name: string }>, res: Response, next: NextFunction) => {
   const { name } = req.params
-  return res.sendFile(path.resolve(UPLOAD_DIR, name), (err) => {
+  return res.sendFile(path.resolve(UPLOAD_IMAGE_DIR, name), (err) => {
+    if (err) {
+      res.status((err as any).status).send('Not found')
+    }
+  })
+}
+export const serveVideoController = (req: Request<{ name: string }>, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  return res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, name), (err) => {
     if (err) {
       res.status((err as any).status).send('Not found')
     }
